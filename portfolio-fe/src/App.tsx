7a6713109a6e556a8420160ab037ba0f10cbdb45
J91-cloud <jessygjerek2@gmail.com>
2025-02-15 19:34:49 -0500

    ee

--- portfolio-fe/src/App.tsx
+++ portfolio-fe/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/navbar/NavBar";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import About from "./pages/About/About";
 import Portfolio from "./pages/Projects/Projects";
 import AcceptComments from "./Components/comments/AcceptComments";
